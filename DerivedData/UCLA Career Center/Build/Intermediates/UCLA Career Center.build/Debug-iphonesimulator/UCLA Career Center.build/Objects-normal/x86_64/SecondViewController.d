8e61db5a8e62904d9be125e873d6d931b65663bd
cecilricky <cecilricky@gmail.com>
2014-02-08 22:35:41 -0800

    Starting work on Contact us feature

---
+++ DerivedData/UCLA Career Center/Build/Intermediates/UCLA Career Center.build/Debug-iphonesimulator/UCLA Career Center.build/Objects-normal/x86_64/SecondViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/cecilricky/Public/UCLA\ Career\ Center/UCLA\ Career\ Center/SecondViewController.m \
+  /Users/cecilricky/Public/UCLA\ Career\ Center/UCLA\ Career\ Center/SecondViewController.h
