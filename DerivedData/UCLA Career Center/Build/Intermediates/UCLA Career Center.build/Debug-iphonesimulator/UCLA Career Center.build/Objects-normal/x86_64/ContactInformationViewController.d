9a92671956b9b3bff1b8e8bfab85d5b3e5f6ec6e
cecilricky <cecilricky@gmail.com>
2014-02-13 19:36:48 -0800

    Revert "Revert f025f8f..6559127"
    This reverts commit 3887bd1b876174a6022f63dd7ad065eef786175a.

---
+++ DerivedData/UCLA Career Center/Build/Intermediates/UCLA Career Center.build/Debug-iphonesimulator/UCLA Career Center.build/Objects-normal/x86_64/ContactInformationViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/cecilricky/Public/UCLA\ Career\ Center/UCLA\ Career\ Center/ContactInformationViewController.m \
+  /Users/cecilricky/Public/UCLA\ Career\ Center/UCLA\ Career\ Center/ContactInformationViewController.h
