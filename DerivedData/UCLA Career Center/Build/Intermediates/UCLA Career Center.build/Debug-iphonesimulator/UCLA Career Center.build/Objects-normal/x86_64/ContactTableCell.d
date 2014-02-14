dbd6a05167bb9e8262e01df0df678d7950fe7e18
cecilricky <cecilricky@gmail.com>
2014-02-13 17:09:02 -0800

    Neglect this commit
    This commit only for testing purpose, use Jamey’s recent commit for work

---
+++ DerivedData/UCLA Career Center/Build/Intermediates/UCLA Career Center.build/Debug-iphonesimulator/UCLA Career Center.build/Objects-normal/x86_64/ContactTableCell.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/cecilricky/Public/UCLA\ Career\ Center/UCLA\ Career\ Center/ContactTableCell.m \
+  /Users/cecilricky/Public/UCLA\ Career\ Center/UCLA\ Career\ Center/ContactTableCell.h
